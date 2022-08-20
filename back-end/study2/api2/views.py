04474e3bdaf54e297adae534450a896450a8b918
leesunuk <leesunook12@naver.com>
2022-08-20 18:05:36 +0900

    Like API

--- back-end/study2/api2/views.py
+++ back-end/study2/api2/views.py
@@ -16,10 +16,10 @@
 #     queryset = get_comment_model().objects.all()
 #     serializer_class = CommentSerializer
 
-from rest_framework.generics import ListAPIView, RetrieveAPIView,CreateAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView,CreateAPIView, UpdateAPIView
 from blog.models import Comment, Post
-from .serializers import CommentSerializer, PostListSerializer, PostRetrieveSerializer
-
+from .serializers import CommentSerializer, PostListSerializer, PostRetrieveSerializer, PostLikeSerializer
+from rest_framework.response import Response
 class PostListAPIView(ListAPIView):
     queryset = Post.objects.all()
     serializer_class = PostListSerializer
@@ -31,3 +31,21 @@ class PostRetrieveAPIView(RetrieveAPIView):
 class CommentCreateAPIView(CreateAPIView):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
+    
+class PostLikeAPIView(UpdateAPIView):
+    queryset = Post.objects.all()
+    serializer_class =PostLikeSerializer
+    
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        data = {'like' : instance.like +1}
+        serializer = self.get_serializer(instance, data=data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+        return Response(data['like'])
