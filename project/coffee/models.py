372aced6e7cf84126093578dcb8208d9ea7c4b66
leesunuk <leesunook12@naver.com>
2022-08-31 19:32:38 +0900

    back : 기존 model 수정, CustomUserModel 밑작업

--- project/coffee/models.py
+++ project/coffee/models.py
@@ -1,5 +1,7 @@
+from contextlib import AbstractContextManager
 from django.contrib.auth import get_user_model
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 
 # Create your models here.
 
@@ -12,7 +14,8 @@ class Post(models.Model):
     content = models.TextField('CONTENT')
     create_dt = models.DateTimeField('CREATE DT', auto_now_add=True)
     update_dt = models.DateTimeField('UPDATE DT', auto_now=True)
-    bookmark = models.ManyToManyField(get_user_model(), blank=True)
+    bookmark_user = models.ManyToManyField(get_user_model(), blank=True)
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
     
     class Meta:
         ordering = ('update_dt',)
@@ -37,4 +40,59 @@ class Material(models.Model):
     
     def __str__(self):
         return self.name
+
+class UserManager(BaseUserManager): #유저 생성시 사용하는 Helper class
+    def create_user(self, email, date_of_birth, password=None):
+        if not email: 
+            raise ValueError('Users must have an email address')
+         #django에서 제공해주는 user모델을 보면 email 파라메터 자리에 username이 있음,
+         # 하지만 필자는 email을 이용해 로그인을 구현할 것임으로 다음과 같이 수정
+        # email을 입력하지 않는 경우 ValueError를 return
+        user = self.model(
+            email = self.normalize_email(email),
+            date_of_birth=date_of_birth,
+        )
+        
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
     
+    def create_superuser(self, email, date_of_birth, password):
+        #위와 같은 사유로 email로 치환
+        user = self.create_user(
+            email,
+            password=password,
+            date_of_birth=date_of_birth,
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+    
+class User(AbstractBaseUser): # 실제 user 모델
+    email = models.EmailField(
+        verbose_name='email',
+        max_length=255,
+        unique=True,
+        
+    )
+    date_of_birth = models.DateField()
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    
+    objects = UserManager()
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['date_of_birth']
+    
+    def __str__(self):
+        return self.email
+    
+    def has_perm(self, perm, obj=None):
+        return True
+    
+    def has_module_perms(self, app_label):
+        return True
+    
+    @property
+    def is_staff(self):
+        return self.is_admin
