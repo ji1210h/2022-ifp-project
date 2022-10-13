80e34a83c32b33766226c7723277f28035160bb8
leesunuk <leesunook12@naver.com>
2022-10-13 15:41:17 +0900

    backend : 수정

--- project/project/settings.py
+++ project/project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-8b!tvz9*s1$i=-h(=vsr290f6#)#jve)8o3=ynqti2wh1bq!v5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['15.165.237.85', '127.0.0.1']
+ALLOWED_HOSTS = ['13.125.61.174', '127.0.0.1']
 
 
 # Application definition
